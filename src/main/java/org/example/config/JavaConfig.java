8d4785dc979c5a3291e776af1320bf06361d36e6
AnatolyBarinov <62007944+AnatolyBarinov@users.noreply.github.com>
2024-01-10 16:49:37 +0300

    Add files via upload

---
+++ src/main/java/org/example/config/JavaConfig.java
@@ -0,0 +1,27 @@
+package org.example.config;
+
+import org.example.profile.SystemProfile;
+import org.example.profile.DevProfile;
+import org.example.profile.ProductionProfile;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Primary;
+
+@Configuration
+public class JavaConfig {
+
+    @Bean
+    @ConditionalOnProperty(name = "netology.profile.dev", havingValue = "true")
+    public SystemProfile devProfile() {
+        return new DevProfile();
+    }
+
+    @Bean
+    @ConditionalOnProperty(name = "netology.profile.dev", havingValue = "false")
+    public SystemProfile prodProfile() {
+        return new ProductionProfile();
+    }
+
+}
